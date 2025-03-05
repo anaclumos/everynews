6d6414e984d98cfeb20951d5517d5ec4f15f1afe
Sunghyun Cho <hey@cho.sh>
2025-03-06 07:59:52 +0900

    feat: Update authentication configuration and add passkey support

---
+++ apps/engine/README.md
@@ -0,0 +1,11 @@
+## When making changes to DB
+
+```
+bun x drizzle-kit generate
+```
+
+## When making changes to Better Auth
+
+```
+bun x @better-auth/cli@latest generate --output ./src/db/schema.ts --config ./src/auth.ts
+```
--- apps/engine/src/auth.ts
+++ apps/engine/src/auth.ts
@@ -1,14 +1,16 @@
-import { betterAuth } from "better-auth";
-import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { db } from "./db";
-import { organization } from "better-auth/plugins";
-import { apiKey } from "better-auth/plugins";
-import { admin } from "better-auth/plugins";
-
+import { betterAuth } from 'better-auth'
+import { drizzleAdapter } from 'better-auth/adapters/drizzle'
+import { admin, apiKey, organization } from 'better-auth/plugins'
+import { passkey } from 'better-auth/plugins/passkey'
+import { db } from './db'
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
-    provider: "pg",
+    provider: 'pg',
   }),
-  plugins: [apiKey(), organization(), admin()],
-});
+  emailAndPassword: {
+    enabled: true,
+  },
+  appName: 'everynews-engine',
+  plugins: [apiKey(), admin(), organization(), passkey()],
+})
