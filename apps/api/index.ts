a76c519a220d2068af99a450836d43664875477d
Sunghyun Cho <hey@cho.sh>
2025-03-22 19:41:59 +0900

    feat: separated auth router

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,10 +1,24 @@
 import { cors } from '@elysiajs/cors'
 import Elysia from 'elysia'
 import { authRouter } from './routes/auth'
-import { docsRouter } from './routes/docs'
+import swagger from '@elysiajs/swagger'
 
 const app = new Elysia()
   .use(cors())
-  .use(docsRouter)
+  .use(
+    swagger({
+      path: '/',
+      exclude: ['/json'], // Swagger itself
+    }),
+  )
+  .get('/health', () => 'OK', {
+    detail: {
+      summary: 'Health Check',
+      tags: ['Health'],
+    },
+  })
+
   .use(authRouter)
   .listen(process.env.BACKEND_PORT || 8000)
+
+export default app
