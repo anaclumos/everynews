f1243f70d537d37350507648bd17e4249c23f8a9
Sunghyun Cho <hey@cho.sh>
2025-03-22 23:22:08 +0900

    refactor: Remove unused OpenAPI merging function

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,6 +1,5 @@
 import { cors } from '@elysiajs/cors'
 import swagger from '@elysiajs/swagger'
-import { mergeOpenAPISpecs } from '@everynews/util'
 import Elysia from 'elysia'
 import { auth } from './auth'
 
@@ -19,37 +18,5 @@ const app = new Elysia()
       path: '/',
     }),
   )
-
-// Add a combined OpenAPI endpoint
-app.get('/openapi.json', async ({ set }) => {
-  // Fetch the auth OpenAPI spec
-  const authSpec = await fetch(
-    'http://localhost:' +
-      (process.env.BACKEND_PORT || 8000) +
-      '/auth/reference/openapi.json',
-  ).then((res) => res.json())
-
-  // Fetch the main API spec
-  const mainSpec = await fetch(
-    'http://localhost:' + (process.env.BACKEND_PORT || 8000) + '/',
-  ).then((res) => res.json())
-
-  // Merge the specs
-  const combinedSpec = mergeOpenAPISpecs({
-    title: 'Everynews Combined API',
-    version: '1.0.0',
-    specs: [
-      { spec: mainSpec },
-      {
-        spec: authSpec,
-        pathPrefix: '/auth',
-        transform: (name: string) => `Auth-${name}`,
-      },
-    ],
-  })
-
-  set.headers['Content-Type'] = 'application/json'
-  return combinedSpec
-})
 
 app.listen(process.env.BACKEND_PORT || 8000)
